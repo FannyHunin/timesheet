f72e95cf6dd0b0b0126f5c868f73095fa5b53f6b
CactusB0y <68423704+CactusB0y@users.noreply.github.com>
2020-10-23 16:37:31 +0200

    v6

--- src/components/ajoutTaches/AjoutTaches.js
+++ src/components/ajoutTaches/AjoutTaches.js
@@ -4,32 +4,46 @@ import "./ajoutTaches.css"
 
 
 const AjoutTaches = () => {
-    const[toDos, setToDos] = useState([
-    ])
+    const[toDos, setToDos] = useState([[],[],[],[]])
+
+    console.log(toDos[1]);
     const inpValue = useRef(null);
     const inpDate = useRef(null);
+    const inpStart = useRef(null);
+    const inpEnd = useRef(null);
     const [pText, setPText] = useState('');
     const [valDate, setValDate] = useState(0);
+    const [valStart, setValStart] = useState(0);
+    const [valEnd, setValEnd] = useState(0);
 
     const addTask = () => {
         let valeurP = inpValue.current.value
         let valeurD = inpDate.current.value
+        let valeurS = inpStart.current.value
+        let valeurE = inpEnd.current.value
         let leP = pText
         let laD = valDate
+        let laS = valStart
+        let laE = valEnd
         let TabToDos = toDos
 
         leP = valeurP
         laD = valeurD
+        laS = valeurS
+        laE = valeurE
         inpValue.current.value = ''
-        TabToDos.push(leP)
+        TabToDos[0].push(leP)
+        TabToDos[1].push(laD)
+        TabToDos[2].push(laS)
+        TabToDos[3].push(laE)
 
         setValDate(laD)
         setPText(leP)
         setToDos(TabToDos)
     }
 
-    const toDosMap = toDos.map ((e, i) => (
-        <Taches inpValue={inpValue} key={i} toDos={e} pText={toDos[i]} pDate={valDate}/>
+    const toDosMap = toDos[0].map ((e, i) => (
+        <Taches inpValue={inpValue} key={i} toDos={e} pText={toDos[0][i]} pDate={toDos[1][i]} pStart={toDos[2][i]} pEnd={toDos[3][i]} />
     ))
     
     
@@ -51,11 +65,11 @@ const AjoutTaches = () => {
                     <div className = "forms2">
                         <label htmlFor="">
                             Entrez une heure de début
-                            <input type="time" name="" id=""/>
+                            <input ref={inpStart} type="time" name="" id=""/>
                         </label>
                         <label htmlFor="">
                             Entrez une heure de fin
-                            <input type="time" name="" id=""/>
+                            <input ref={inpEnd} type="time" name="" id=""/>
                         </label>
                     </div>
                </div>
--- src/components/taches/Taches.js
+++ src/components/taches/Taches.js
@@ -2,12 +2,14 @@ import userEvent from '@testing-library/user-event';
 import React, {useRef} from 'react';
 import "./taches.css"
 
-const Taches = ({pText, pDate}) => {
+const Taches = ({pText, pDate, pStart, pEnd}) => {
     
     return (
         <div className="taches">
             <p> {pText} </p>
-            <p> {pDate}</p>
+            <p>{pDate}</p>
+            <p>{pStart}</p>
+            <p>{pEnd}</p>
             <div>
                 <button className="btnTaches">Valider</button>
                 <button className="btnTaches">Supprimer</button>

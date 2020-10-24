cd331990720d06854183f6781fcbb27a941e6f2b
FannyHunin <fanny.hunin.an123@gmail.com>
2020-10-24 13:33:20 +0200

    modifications css

--- src/components/taches/Taches.js
+++ src/components/taches/Taches.js
@@ -6,10 +6,10 @@ const Taches = ({pText, pDate, pStart, pEnd}) => {
     
     return (
         <div className="taches">
-            <p> {pText} </p>
-            <p>{pDate}</p>
-            <p>{pStart}</p>
-            <p>{pEnd}</p>
+            <h3 className="has-text-white has-text-weight-bold is-size-3">{pText}</h3>
+            <h4 className="subtitle has-text-white is-size-4">Date : {pDate}</h4>
+            <p>Heure de début : {pStart}</p>
+            <p>Heure de fin : {pEnd}</p>
             <div>
                 <button className="btnTaches">Valider</button>
                 <button className="btnTaches">Supprimer</button>
